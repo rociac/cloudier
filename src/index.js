9b2001909bfc3c856a5ed85a02262cde6ad4ad7e
Rodolfo <brsrkdev@gmail.com>
2020-01-10 20:35:26 -0600

    Add form validation and error handling, Fix styling

--- src/index.js
+++ src/index.js
@@ -1,36 +1,63 @@
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 import './main.scss';
+import domManager from './js/info';
 
 const APIKEY = 'eaed0043eb661ba7a7f56160196f9ad9';
-const btn = document.querySelector('.btn-search');
-
-
+const btn = document.querySelector('.search__btn');
+const search = document.getElementById('search__input');
+const form = document.querySelector('.search');
+const error = document.querySelector('.error');
 
 async function getWeather(location, unit) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${APIKEY}&units=${unit}`, { mode: 'cors' });
-  const weatherData = await response.json();
-  console.log(weatherData);
-  const icon = document.createElement('img');
-  icon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
   const container = document.querySelector('.info');
-  container.appendChild(icon);
-  const temperature = document.querySelector('.info__temper');
-  temperature.insertAdjacentHTML('beforeend', weatherData.main.temp.toFixed(0) + '°');
-  const city = document.querySelector('.info__city');
-  city.insertAdjacentHTML('beforeend', weatherData.name);
+  container.innerHTML = "";
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${APIKEY}&units=${unit}`, { mode: 'cors' });
+    const weatherData = await response.json();
+    domManager.setBackground(weatherData.weather[0].main);
+    const infoElements = domManager.setInfo(weatherData);
+    container.innerHTML += infoElements;
+    const icon = document.createElement('img');
+    const iconContainer = document.querySelector('.info__bottom-right');
+    icon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    iconContainer.appendChild(icon);
+  } catch (e) {
+    alert('Enter a valid city');
+  }
 }
 
 function searchLocation() {
-  const location = document.getElementById('search').value.trim();
+  const location = document.getElementById('search__input').value.trim();
   const unit = document.getElementById('units').value;
   getWeather(location, unit);
 }
 
 const init = () => {
-  btn.addEventListener('click', ev => {
-    ev.preventDefault();
-    searchLocation();
-    document.getElementById('search').value = '';
+  btn.addEventListener('click', event => {
+    if (search.validity.valueMissing) {
+      error.innerHTML = 'You must enter a city name';
+      error.classList = 'error active';
+      event.preventDefault();
+    } else {
+      event.preventDefault();
+      searchLocation();
+      document.getElementById('search__input').value = '';
+    }
   }, false);
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+search.addEventListener('input', () => {
+  if (!search.validity.valueMissing) {
+    error.innerHTML = '';
+    error.className = 'error';
+  }
+}, false);
+
+form.addEventListener('submit', (event) => {
+
+}, false);
---
+++ src/js/info.js
@@ -0,0 +1,53 @@
+const domManager = (() => {
+  const body = document.querySelector('body');
+
+  const setInfo = (weatherData) => {
+    const infoElements = `
+  <div class="info__top">${weatherData.name}, ${weatherData.sys.country}</div>
+  <div class="info__bottom">
+    <div class="info__bottom-left">
+      <p class="temp">${weatherData.main.temp.toFixed(0)}°</p>
+      <p class="weather">${weatherData.weather[0].main}</p>
+      <p class="feels-like">Feels like <span>${weatherData.main.feels_like}°</span></p>
+      <p class="range">H <span>${weatherData.main.temp_max.toFixed(0)}°</span> / L <span>${weatherData.main.temp_min.toFixed(0)}°</span></p>
+    </div>
+    <div class="info__bottom-right">
+    </div>
+  </div>`;
+    return infoElements;
+  }
+
+  const setBackground = (weatherData) => {
+    switch (weatherData) {
+      case 'Clouds':
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1444402185522-47a923ee3e25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)';
+        break;
+      case 'Thunderstorm':
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1509401934319-cb35b87bf39e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)';
+        break;
+      case 'Drizzle':
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1541919329513-35f7af297129?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjQzMzEwfQ&auto=format&fit=crop&w=1350&q=80)';
+        break;
+      case 'Rain':
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1486016006115-74a41448aea2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80)';
+        break;
+      case 'Snow':
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1491002052546-bf38f186af56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1383&q=80)';
+        break;
+      case 'Clear':
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1518717202715-9fa9d099f58a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1401&q=80)';
+        break;
+      default:
+        body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1482841628122-9080d44bb807?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80)';
+        break;
+    }
+  }
+
+  return {
+    setInfo,
+    setBackground,
+  }
+
+})();
+
+export default domManager;
